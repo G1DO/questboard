f15a6ce2bee2e03cecaa85f4fa8606e83bb26388
Ibrahim <ibrahimmohamedabdelsadek@gmail.com>
2025-10-20 18:54:09 +0300

    Change port to 8080 for Elastic Beanstalk

--- wsgi.py
+++ wsgi.py
@@ -6,4 +6,4 @@ app = create_app()
 
 if __name__ == "__main__":
     # Dev-only runner. In production we will use gunicorn.
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
